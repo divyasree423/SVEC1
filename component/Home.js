bdc13189e06874b43d0af782aae3c01ab9d72518
divyasree423 <96712124+divyasree423@users.noreply.github.com>
2022-01-01 12:40:02 +0530

    reactjs

--- component/Home.js
+++ component/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { BrowserRouter,Routes,Route,Link } from 'react-router-dom';
 import FormRendering from './FormRendering';
 import ListRendering from './ListRendering';
 import StateinClassComponent from './StateinClassComponent';
@@ -8,70 +8,39 @@ import UnionBank from './UnionBank';
 
 export default function Home() {
     return (
-        <>
+     <>
         <div>
         <BrowserRouter>
         <Routes>
-            <Route path="/home" element={<StateInFunctionalComponent/>}></Route>
+          
+            <Route path="/home" element={<Main/>}></Route>
+            <Route path="/login" element={<FormRendering/>}></Route>
            
-        </Routes>
-        </BrowserRouter>
-        </div>
-        <div>
-            <BrowserRouter>
-            <Routes>
-                <Route path="/home" element={<Main/>}></Route>
-               
-            </Routes>
-            </BrowserRouter>
-            
-        </div>
-    
-        <div>
-        <BrowserRouter>
-        <Routes>
-            <Route path="/home" element={<FormRendering/>}></Route>
-           
-        </Routes>
-        </BrowserRouter>
-        </div>
-        <div>
-        <BrowserRouter>
-        <Routes>
+            <Route path="/home" element={<FormRendering/>}></Route>   
             <Route path="/home" element={<ListRendering/>}></Route>
-           
-        </Routes>
-        </BrowserRouter>
-        </div>
-        <div>
-        <BrowserRouter>
-        <Routes>
             <Route path="/home" element={<StateinClassComponent/>}></Route>
+            <Route path="/home" element={<StateInFunctionalComponent/>}></Route>
+            
            
         </Routes>
         </BrowserRouter>
-        </div>
-        <div>
-        <BrowserRouter>
-        <Routes>
-            <Route path="/home" element={<UnionBank/>}></Route>
-           
-        </Routes>
-        </BrowserRouter>
-        </div>
+        {/* <FormRendering/>
+        <ListRendering/>
+        <StateinClassComponent/>
+        <StateInFunctionalComponent/> */}
+    </div>
         
-        </>
-        
-    
-    )
+    </>      
+ )
 }
 
 function Main(){
-    return(
+    return (
         <>
         <h1>
             Main 
         </h1>
+        <Link to="/login">Login</Link>
         </>
     )
 }
